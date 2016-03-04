f1423b410dd689c5dbba5356809158938acaa6eb
kuzuya <udai928@gmail.com>
2016-03-04 11:06:08 +0900

    標準入力実装途中

--- abc033_b.rb
+++ abc033_b.rb
@@ -1,13 +1,21 @@
-num = 4
+#num = 4
+num = gets.to_i
+strs = ""
+for n in 1..num
+  p strs
+  strs += gets.to_s.split(" ")
+end
 
-str1 = "unagi 20"
-str2 = "usagi 13"
-str3 = "snuke 42"
-str4 = "smeke 7"
+# str1 = "unagi 20"
+# str2 = "usagi 13"
+# str3 = "snuke 42"
+# str4 = "smeke 7"
+#
+# strs = str1.split(" ") + str2.split(" ") + str3.split(" ") + str4.split(" ")
 
-ary = str1.split(" ") + str2.split(" ") + str3.split(" ") + str4.split(" ")
-
-hash = Hash[*ary]
+p strs
+p "--------------------------"
+hash = Hash[*strs]
 hash.each_key{|key|
   hash.store(key,hash.fetch(key).to_i)
 }
