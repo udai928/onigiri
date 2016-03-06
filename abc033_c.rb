56582396cc566053c9328be5bbc494c9e23a94e8
kuzuyayuudai <udai928@gmail.com>
2016-03-06 23:15:34 +0900

    問題のアプローチの仕方を考えないといけないと悟った。

--- abc033_c.rb
+++ abc033_c.rb
@@ -2,50 +2,14 @@ require_relative "read_file.rb"
 include Tools
 
 str = Tools.get_input_online
-str_splited =  str.split("")
+str_splited =  str.split("+")
 
 # str_splited = gets.chomp.split("")
 
-ASTER = "*"
-PLUS = "+"
-count_multipl = 0
-count_plus = 0
-
-str_splited.each_with_index{|char,index|
-    if (index == 1 and char == ASTER) or (index > 1 and char == ASTER and str_splited.fetch(index - 2) != ASTER)
-        if str_splited.fetch(index - 1) == "0" and str_splited.fetch(index + 1) == "0"
-        else
-            index_check_plus = index
-            index_of_firstzero = 0
-            while index_check_plus < str_splited.size and str_splited.fetch(index_check_plus) != "+"
-                if str_splited.fetch(index_check_plus) == "0" and index_of_firstzero == 0
-                    index_of_firstzero = index_check_plus
-                    break
-                end
-                index_check_plus += 1
-            end
-            if index_of_firstzero == 0
-                str_splited[index - 1] = "0"
-                count_multipl+=1
-            end
-        end
+count = 0
+str_splited.each_with_index{|item,index|
+    if item.index("0") == nil
+        count+=1
     end
 }
-
-while str_splited.include?("*") == true
-    str_splited.each_with_index{|char,index|
-        if char == ASTER
-            calc_result = str_splited.fetch(index - 1).to_i * str_splited.fetch(index + 1).to_i
-            str_splited.slice!(index - 1,3)
-            str_splited.insert(index - 1,calc_result.to_s)
-            break
-        end
-    }
-end
-
-str_splited.each_with_index{|char,index|
-    if index % 2 == 0 and char != "0"
-        count_plus += 1
-    end
-}
-puts count_plus+count_multipl
+p count
