c8bc62fe5e847299de22acc6a17cc5ad33235bc1
kuzuyayuudai <udai928@gmail.com>
2016-03-06 23:02:54 +0900

    驚きの処理フローに気づく直前

--- abc033_c.rb
+++ abc033_c.rb
@@ -12,33 +12,15 @@ count_multipl = 0
 count_plus = 0
 
 str_splited.each_with_index{|char,index|
-    if index > 1 and char == ASTER
-        multiplication_index = index
-        if str_splited.fetch(index - 2) != ASTER and str_splited.fetch(index - 1) != "0" and str_splited.fetch(index + 1) != "0"
-            while multiplication_index < str_splited.size
-                if str_splited.fetch(multiplication_index) != "+"
-                    index_of_firstzero = 0
-                    if str_splited.fetch(multiplication_index) == "0" and index_of_firstzero == 0
-                        index_of_firstzero = multiplication_index
-                        break
-                    end
-                end
-                multiplication_index += 1
-            end
-            if index_of_firstzero == 0
-                str_splited[index - 1] = "0"
-                count_multipl+=1
-            end
-        end
-    elsif index == 1 and char == ASTER
-        if str_splited.fetch(index - 1) != "0" and str_splited.fetch(index + 1) != "0"
+    if (index == 1 and char == ASTER) or (index > 1 and char == ASTER and str_splited.fetch(index - 2) != ASTER)
+        if str_splited.fetch(index - 1) == "0" and str_splited.fetch(index + 1) == "0"
+        else
             index_check_plus = index
-            while index_check_plus < str_splited.size
-                if str_splited.fetch(index_check_plus) != "+"
-                    index_of_firstzero = 0
-                    if str_splited.fetch(index_check_plus) == "0" and index_of_firstzero == 0
-                        index_of_firstzero = index_check_plus
-                    end
+            index_of_firstzero = 0
+            while index_check_plus < str_splited.size and str_splited.fetch(index_check_plus) != "+"
+                if str_splited.fetch(index_check_plus) == "0" and index_of_firstzero == 0
+                    index_of_firstzero = index_check_plus
+                    break
                 end
                 index_check_plus += 1
             end
