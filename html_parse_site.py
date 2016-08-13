977de123034c8322d6ae892ac0a5bdab1be7f70d
kuzuyayuudai <udai928@gmail.com>
2016-08-13 14:44:47 +0900

    定数を外だし

--- html_parse_site.py
+++ html_parse_site.py
@@ -1,18 +1,32 @@
-# -*-config:utf-8-*-
+# coding:utf-8
 
 from bs4 import BeautifulSoup
 import urllib.request
 import time
 
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formatdate
+
+ACCOUNT = ''
+To = ''
+PASSWORD = ''
+SUB = 'wear更新通知（ショーパン+166~170cm+更新順）'
+URL = "http://wear.jp/sp/women-coordinate/?search_word=%E3%82%B7%E3%83%A7%E3%83%BC%E3%83%91%E3%83%B3&from_height=166&to_height=170&type_id=2"
+
+
 def main():
     start_time = time.time()
     print ("START html_parse from wear.",time.ctime())
-    url = "http://hogehoge.com"
-    soup = html_parser_from_wear(url)
-    html_scraping_from_soup(soup)
+    
+    soup = html_parser_from_wear(URL)
+    body = html_scraping_from_soup(soup)
+    send_message_using_smtp_ssl(SUB,body)
+
     end_time = time.time()
     print ("END html_parse from wear. ", time.ctime())
     print ("Processing time was %f" %(end_time - start_time) +"s.")
+
 
 def html_parser_from_wear(url_str):
 
@@ -22,8 +36,8 @@ def html_parser_from_wear(url_str):
     response = urllib.request.urlopen(req)
     html = response.read()
     soup = BeautifulSoup(html, 'lxml')
-
     return soup
+
 
 def html_scraping_from_soup(bs4_soup):
 # スクレイピングするだけの関数にしたいけど、更新チェック関数をfor文で回すところどうしようかな。
@@ -34,17 +48,23 @@ def html_scraping_from_soup(bs4_soup):
         class_time = class_meta_clearfix.find_all('p', class_ = 'time')
         str_class_time = str(class_time).replace('<p class="time">','').replace('</p>','')
         result = wear_update_check(str_class_time)
+        if result is '更新はありませんでした。':
+            pass
+        else:
+            break
     print (result)
+    return result
+
 
 def wear_update_check(str_class_time):
 
     if '分前' in str_class_time:
-        message = (str_class_time + 'に更新がありました。ちぇけらψ(｀∇´)ψ')
+        message = (str_class_time + 'に更新がありました。ちぇけらψ(｀∇´)ψ¥n¥n' + URL)
         return message
     elif '時間前' in str_class_time:
-      int_class_time = int(str_class_time.replace('時間前', ''))
-      if int_class_time > 5:
-          print (str_class_time + 'に更新がありました。ちぇけらψ(｀∇´)ψ')
+      int_class_time = int(str_class_time.replace('時間前]', '').replace('[',''))
+      if int_class_time <= 10: #10時間以内に更新があったかどうか。あったら通知用に本文をセット。
+          message = (str_class_time + 'に更新がありました。ちぇけらψ(｀∇´)ψ¥n¥n' + URL)
           return message
       else:
           pass
@@ -52,12 +72,30 @@ def wear_update_check(str_class_time):
         pass
     return '更新はありませんでした。'
 
-    # class_name = class_meta_clearfix.find_all('p', class_ = 'name')
-    # class_txt =  class_meta_clearfix.find_all('p', class_ = 'txt')     
-    # str_class_name = str(class_name).replace('<p class="name">','').replace('</p>','')
-    # str_class_txt = str(class_txt).replace('<p class="txt">','').replace(',  JP</p>','')
 
-    # print (str_class_name, str_class_txt, str_class_time)
+def create_message(subject,body):
+    msg = MIMEText(body)
+    msg["Subject"] = subject
+    msg["From"] = ACCOUNT
+    msg["To"] = To
+    msg["Date"] = formatdate(localtime=True)
+    return msg
+
+
+def send_message_using_smtp_ssl(subject,body):
+    if '更新はありませんでした。' in body :
+        pass
+    else:
+        msg = create_message(subject,body)
+
+        print (subject,body)
+
+        s = smtplib.SMTP_SSL("smtp.gmail.com")
+        s.login(ACCOUNT, PASSWORD)
+
+        s.send_message(msg)
+        s.quit()
+
 
 if __name__ == '__main__':
-	main()
+    main()
